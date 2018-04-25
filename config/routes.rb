fc34fcf6d2634557b410df0a35f2a94e5cfbc29d
Tom Spettigue <apickle@gmail.com>
2018-04-25 11:24:52 -0600

    updated Gemfile, scss, added downvote button, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :topics
+  root 'topics#index'
+  resources :topics do
+    member do
+      post 'downvote'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
